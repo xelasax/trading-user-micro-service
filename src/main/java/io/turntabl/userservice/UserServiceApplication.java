19a740b045ce81162504e16b3bd3f007ab732967
Quame Work PC <quamelexiz@gmail.com>
2021-12-01 19:47:21 +0000

    Updated Project with Spring OAUTH2 Security

--- src/main/java/io/turntabl/userservice/UserServiceApplication.java
+++ src/main/java/io/turntabl/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package io.turntabl.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+//@EnableWebSecurity
 public class UserServiceApplication {
 
     public static void main(String[] args) {
