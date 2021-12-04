0aff6e11db7d8168c69390d08bee42a07560eb57
Quame <quamelexiz@gmail.com>
2021-12-04 19:50:00 +0000

    Updated Controllers to accommodate JWT headers

--- src/main/java/io/turntabl/userservice/frontend/request/UserRequest.java
+++ src/main/java/io/turntabl/userservice/frontend/request/UserRequest.java
@@ -1,8 +1,12 @@
 package io.turntabl.userservice.frontend.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
     private String id;
     private String email;
