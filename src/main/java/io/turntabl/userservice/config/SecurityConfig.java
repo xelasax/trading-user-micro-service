74929d1f2ac611a29ff484a93801d333255588f1
Quame <quamelexiz@gmail.com>
2021-12-07 11:11:36 +0000

    Added frame options

--- src/main/java/io/turntabl/userservice/config/SecurityConfig.java
+++ src/main/java/io/turntabl/userservice/config/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+        http.headers().frameOptions().disable();
     }
 
 }
